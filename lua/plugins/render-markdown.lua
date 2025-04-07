d66aa69984336f6534a27967cacdc2ae6caa46c0
yahia-mazouzi <yahia.mazouzi.se@gmail.com>
2025-04-07 17:02:24 +0100

    add render-markdown plugin

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  ft = { "markdown" },
+
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+
+  config = function()
+    require("render-markdown").setup {}
+  end,
+  opts = {},
+}

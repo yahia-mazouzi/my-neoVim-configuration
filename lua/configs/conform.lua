6f00d84798edd0edc21399932f84742544dec236
yahia-mazouzi <yahia.mazouzi.se@gmail.com>
2025-04-07 15:30:18 +0100

    add formatters

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,23 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    typescript = { "prettier" },
+    javascript = { "prettier" },
+    go = { "gofumpt" },
+    python = { "black" },
+    c_cpp = { "clang-format" },
+    cpp = { "clang_format" },
+    c = { "clang_format" },
+    java = { "google-java-format" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options

47c736d569f7fcf086d6b51e7e00af7b9b8cb61f
yahia-mazouzi <yahia.mazouzi.se@gmail.com>
2025-04-08 01:28:33 +0100

    optimize the treesitter plugin to avoid breaking in large files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,13 +13,39 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "lua",
+          "json",
+          "bash",
+          "html",
+          "css",
+          "javascript",
+          "typescript",
+          "python",
+          "markdown",
+          "markdown_inline",
+          "yaml",
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+          disable = function(lang, buf)
+            local max_filesize = 150 * 1024 -- 150kb
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+            return false
+          end,
+        },
+        indent = {
+          enable = true, -- Enable Treesitter-based indentation
+        },
+      }
+    end,
+  },
 }

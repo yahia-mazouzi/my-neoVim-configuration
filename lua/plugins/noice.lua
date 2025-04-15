d94c50002346ad4519617ed4155657eb4cde8eb9
yahia-mazouzi <yahia.mazouzi.se@gmail.com>
2025-04-15 23:45:49 +0100

    customize : noice plugin

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,9 +1,77 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
+  opts = {},
+  config = function()
+    require("noice").setup {
+      cmdline = {
+        view = "cmdline_popup", -- or cmdline
+        format = {
+          cmdline = { icon = "" },
+          search_down = { icon = " " },
+          search_up = { icon = " " },
+        },
+      },
+      messages = {
+        view = "notify",
+        view_error = "notify",
+        view_warn = "notify",
+        view_history = "messages",
+      },
+      popupmenu = {
+        enabled = true,
+        backend = "nui",
+      },
+      lsp = {
+        progress = {
+          enabled = true,
+          format = "lsp_progress",
+          format_done = "lsp_progress_done",
+          throttle = 1000 / 30,
+          view = "mini",
+        },
+        signature = {
+          enabled = true,
+          auto_open = {
+            enabled = true,
+            trigger = true,
+            luasnip = true,
+          },
+        },
+        hover = {
+          enabled = true,
+          view = nil, -- default to builtin
+        },
+        message = {
+          enabled = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = false }, -- hide "written" messages
+        },
+      },
+    }
+    require("notify").setup {
+      stages = "fade_in_slide_out",
+      timeout = 2000,
+      max_width = 40,
+      max_height = 5,
+      minimum_width = 20,
+    }
+  end,
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",

d1696a5a91d88543fde4b8094a831ef6e2923445
yahia-mazouzi <yahia.mazouzi.se@gmail.com>
2025-06-29 14:57:45 +0100

    add Aerial for better code nav

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/aerial.nvim",
+  lazy = false,
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("aerial").setup {
+      on_attach = function(bufnr)
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    }
+    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+  end,
+}

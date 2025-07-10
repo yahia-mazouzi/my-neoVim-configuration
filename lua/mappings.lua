79127b69814c4835b9ac7898044b59eda56296d3
yahia-mazouzi <yahia.mazouzi.se@gmail.com>
2025-07-10 19:25:22 +0100

    add mapping for vertical defintion

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "gv", function()
+  vim.cmd "vsplit"
+  vim.lsp.buf.definition()
+end, { desc = "Go to definition in vsp" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

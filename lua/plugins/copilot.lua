7880fbffa17ddf7f1465631c9496038ec5663e02
yahia-mazouzi <yahia.mazouzi.se@gmail.com>
2025-09-04 14:30:00 +0100

    chore : use more copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,10 +5,36 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false }, -- disable ghost text
-        panel = { enabled = false },      -- disable side panel
-      })
+      require("copilot").setup {
+        suggestion = { enabled = false }, -- disable inline ghost text
+        panel = { enabled = false }, -- disable side panel
+        filetypes = {
+          ["*"] = true, -- enable for all filetypes
+        },
+        copilot_node_command = "node", -- ensure node is used
+        server_opts_overrides = {
+          advanced = {
+            completions = {
+              -- Fire Copilot suggestions more often
+              triggerCharacters = {
+                ".",
+                ":",
+                "(",
+                "[",
+                "{",
+                ",",
+                " ",
+                "<",
+                "=",
+                '"',
+                "'",
+                "/",
+              },
+              debounceMs = 75, -- faster refresh (default ~300ms)
+            },
+          },
+        },
+      }
     end,
   },
 
@@ -27,10 +53,11 @@ return {
         optional = true,
         ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
+          -- Put Copilot suggestions *above* LSP and others
           table.insert(opts.sources, 1, {
             name = "copilot",
             group_index = 1,
-            priority = 100,
+            priority = 1000,
           })
         end,
       },
